fe06cba10ea5e64587adb61bc0bed482ee98b7a4
tanimon <8575113+tanimon@users.noreply.github.com>
2021-09-12 13:05:40 +0900

    Solve quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,16 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
+const DISCOUNT_QUANTITY_THRESHOLD: i32 = 40;
+const UNIT_PRICE: i32 = 2;
 
-// Put your function here!
-// fn ..... {
+fn calculate_apple_price(quantity: i32) -> i32 {
+    if quantity > DISCOUNT_QUANTITY_THRESHOLD {
+        quantity
+    } else {
+        quantity * UNIT_PRICE
+    }
+}
 
 // Don't modify this function!
 #[test]
