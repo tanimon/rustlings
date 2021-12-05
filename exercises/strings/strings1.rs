6ff281f96258124a6edd65e1a9c94a1ce0adf131
tanimon <8575113+tanimon@users.noreply.github.com>
2021-12-05 16:30:47 +0900

    Solve strings1

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -2,13 +2,15 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings1` for hints ;)
 
-// I AM NOT DONE
-
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    // Using `to_string` to convert the `&str` to `String`
+    "blue".to_string()
+
+    // Using `String::from` to convert the `&str` to `String`
+    // String::from("blue")
 }
